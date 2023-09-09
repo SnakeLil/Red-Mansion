f4bb5b82e031d574aefa352185e7100eb4a210b4
lilsnake <13192801+lilsnake@user.noreply.gitee.com>
2023-09-09 12:50:36 -0400

    i

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
+
+      
+      App
     </div>
   );
 }
---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Profile.scss'
+export default function Profile() {
+  return (
+    <div className='profile'>
+        Profile
+    </div>
+  )
+}
